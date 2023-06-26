c85fea37726e0abb3ebb3254078b6317167be387
manojdx <manojmca15@gmail.com>
2023-06-26 12:03:52 +0530

    xml and java files

--- app/src/main/java/com/example/gitstart/MainActivity.java
+++ app/src/main/java/com/example/gitstart/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.gitstart;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView testTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        testTxt = findViewById(R.id.test);
+        testTxt.setText("hello1");
     }
 }
